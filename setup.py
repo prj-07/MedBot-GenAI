cf3542db405e5e06492ddee0ed734144f4a7d940
pratyushr <pratyushr@nvidia.com>
2025-09-15 00:26:04 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Pratyush Raj',
+    author_email= 'workprj07@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
